bac97cef71412b33bf49a2efcc7df3fac34d3639
Danilo <08nilo02@gmail.com>
2022-07-29 17:20:58 -0500

    Clase1

---
+++ Clase1/index.js
@@ -0,0 +1,67 @@
+/* 
+//formas recomendadas
+let nombre = "Danilo";
+let direccion = 'calle San Juan de Dios'
+const edad = 84;
+
+//formas no recomendadas
+var apellido = "Espinoza";
+
+console.log(apellido);
+console.log(edad);  
+*/
+
+/* 
+let numero1 = 10;
+let numero2 = 20;
+
+//variables con camelCase y constantes en mayuscula
+let resultadoSuma = numero1 + numero2;
+let resultadoResta = numero1 - numero2;
+let resultadoMultiplicacion = numero1 * numero2;
+
+//Usualmente las constantes se colocan en mayusculas
+const FACTOR = 5;
+
+console.log(resultadoSuma + FACTOR);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+*/
+
+/* let nombre = "Diana";
+let apellido = " Ruelas";
+
+console.log(nombre + apellido);
+console.log(nombre + 1); */
+
+/* let nombre = prompt("Ingresa tu nombre");
+console.log(nombre); */
+
+
+/* let num1 = prompt("ingrese el numero 1");
+let num2 = prompt("ingrese el numero 2");
+
+console.log(parseInt(num1) + parseInt(num2)); */
+
+
+
+/* let algo = 3;
+
+console.log(typeof(algo)); */
+
+/* let entrada = prompt("Ingrese una letra");
+let salida = entrada + " " + "ingresada";
+
+alert(salida); */
+
+let nombre = (prompt("Ingrese su nombre"));
+let apellido = (prompt("Ingrese su apellido"));
+let nombreCompleto = "Tu nombre es " + nombre + " " + apellido;
+
+alert(nombreCompleto);
+
+
+
+
+
+
